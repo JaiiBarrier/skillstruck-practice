5817c9c05d876bc76ce334c0c35d0e15aad4eab4
JaiiBarrier <109163810+JaiiBarrier@users.noreply.github.com>
2022-12-05 14:54:04 -0600

    exported from Skill Struck

---
+++ myscript.js
@@ -0,0 +1,14 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    
+    
+    names = ["Jaii", "Cody," "Kelly"];
+    names.random();
+    
+
+
+/******Don't change the code below*******/    
+}
