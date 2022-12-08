2701c7e3d0ef05df8f2fffde697f1ff252a588b0
JaiiBarrier <109163810+JaiiBarrier@users.noreply.github.com>
2022-12-08 12:08:48 -0600

    exported from Skill Struck

--- myscript.js
+++ myscript.js
@@ -1,14 +1,9 @@
-function whosPaying(names) {
-    
-/******Don't change the code above*******/
-    
-    //Write your code here.
-    
-    
-    names = ["Jaii", "Cody," "Kelly"];
-    names.random();
-    
-
-
-/******Don't change the code below*******/    
+function longestString() {
+  let longest = '';
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i].length > longest.length) {
+      longest = arguments[i];
+    }
+  }
+  return longest;
 }
